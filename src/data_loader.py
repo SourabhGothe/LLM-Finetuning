bdc420f68a0917e0a49a1e753913a2cb4e7f22ce
SourabhGothe <95920510+SourabhGothe@users.noreply.github.com>
2025-06-14 21:00:12 +0530

    Update data_loader.py

--- src/data_loader.py
+++ src/data_loader.py
@@ -9,7 +9,7 @@ project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 if project_root not in sys.path:
     sys.path.insert(0, project_root)
 
-from datasets import load_dataset, DatasetDict
+from datasets import load_dataset, DatasetDict, Dataset
 from src.linearize import get_linearizer
 from omegaconf import DictConfig
 
